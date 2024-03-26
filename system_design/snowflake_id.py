e67eab30e6dbc7d1db2de03629a67c14e95be639
QY0831 <aug_31@163.com>
2024-03-26 20:57:40 +0800

    update

---
+++ system_design/snowflake_id.py
@@ -0,0 +1,41 @@
+"""
+推特snowflake算法:
+分布式系统中的唯一ID生成器
+"""
+import time
+
+class Snowflake:
+    def __init__(self, machine_id):
+        self.machine_id = machine_id
+        self.sequence = 0
+        self.last_timestamp = -1
+
+    def generate_id(self):
+        timestamp = int(time.time() * 1000)
+
+        if timestamp < self.last_timestamp:
+            raise Exception("Invalid system clock")
+
+        if timestamp == self.last_timestamp:
+            self.sequence = (self.sequence + 1) & 4095 # 4095 = 0b111111111111
+            if self.sequence == 0:  # sequence溢出，需要等待timestamp更新
+                timestamp = self.wait_next_millis(self.last_timestamp)
+        else:
+            self.sequence = 0
+
+        self.last_timestamp = timestamp
+
+        # 生成Snowflake ID
+        snowflake_id = ((timestamp - 1546300800000) << 22) | (self.machine_id << 12) | self.sequence
+        return snowflake_id
+
+    def wait_next_millis(self, last_timestamp):
+        timestamp = int(time.time() * 1000)
+        while timestamp <= last_timestamp:
+            timestamp = int(time.time() * 1000)
+        return timestamp
+
+# 示例用法
+snowflake = Snowflake(machine_id=1)
+unique_id = snowflake.generate_id()
+print(unique_id)
---
+++ system_design/tiny_url.py
@@ -0,0 +1,62 @@
+# REST API:
+# 缩短URL:
+# POST api/v1/data/shorten
+# 重定向URL:
+# GET api/v1/shortUrl
+import string
+
+BASE62_CHARS = string.digits + string.ascii_lowercase + string.ascii_uppercase
+# 设tiny url长度为N, 则支持62^N个地址
+
+"""       
+输入：长URL --> 长URL在数据库中 --> 生成一个新ID --> 把ID转化为短URL --> 将ID、短URL、长URL保存到数据库
+                |                      
+                |                      
+                +---------是---->返回短URL               
+"""
+class TinyURL:
+    def __init__(self):
+        self.url_to_id = {}
+        self.id_to_url = {}
+        self.current_id = 0
+
+    def encode(self, long_url):
+        if long_url in self.url_to_id:
+            return self.convert_to_base62(self.url_to_id[long_url])
+
+        short_url = self.convert_to_base62(self.current_id)
+        self.url_to_id[long_url] = self.current_id
+        self.id_to_url[self.current_id] = long_url
+        self.current_id += 1
+
+        return short_url
+
+    def decode(self, short_url):
+        id = self.convert_from_base62(short_url)
+        return self.id_to_url.get(id)
+
+    def convert_to_base62(self, num):
+        if num == 0:
+            return BASE62_CHARS[0]
+
+        base62 = ''
+        while num > 0:
+            num, remainder = divmod(num, 62) # 不断对62取模，得到对应的字符
+            base62 = BASE62_CHARS[remainder] + base62
+
+        return base62
+
+    def convert_from_base62(self, base62):
+        num = 0
+        for char in base62:
+            num = num * 62 + BASE62_CHARS.index(char)
+
+        return num
+
+# 示例用法
+tiny_url = TinyURL()
+long_url = "https://www.example.com/long-url"
+short_url = tiny_url.encode(long_url)
+print("Short URL:", short_url)
+decoded_url = tiny_url.decode(short_url)
+print("Decoded URL:", decoded_url)
